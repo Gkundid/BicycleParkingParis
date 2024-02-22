1457c54a0b831f6f57d89aa383f9a8f3fd5521ec
Gab <kundid.gabriel@hotmail.com>
2024-02-22 18:02:20 +0100

    refactoring

--- data_processing/Upload_database.py
+++ data_processing/Upload_database.py
@@ -9,26 +9,19 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # Complete path directory for file configuration
 config_file_path = os.path.join(current_directory, "Credentials-Neo4j-Instance.txt")
 
-# Chemin absolu du fichier CSV dans le même répertoire que Write_database.py
 csv_file_path = os.path.join(current_directory, "bicycle_parking_paris.csv")
 
 def Create_nodes(csv_file_path):    
     try:
-        # Lecture du fichier CSV
         with open(csv_file_path, 'r', newline='') as csvfile:
                 reader = csv.DictReader(csvfile)
-                # Parcours des lignes du fichier CSV
                 for index, row in enumerate(reader, start=1):
-                    # Extraction des propriétés
                     properties = {key: value for key, value in row.items()}
-                    # Ajout de la propriété ID et incrémentation
                     properties["ID"] = index
 
-                    # Construction de la requête Cypher avec les propriétés correctement formatées
                     properties_str = "{" + ", ".join([f"{key}: '{value}'" for key, value in properties.items()]) + "}"
                     query = f"MERGE (p:ParkingVelo {properties_str})"
 
-                    # Exécution de la requête Cypher
                     summary = driver.execute_query(query, database_="neo4j").summary
 
                     print("Created {nodes_created} nodes in {time} ms.".format(
@@ -40,19 +33,14 @@ def Create_nodes(csv_file_path):
         # further logging/processing
 
 
-# Chargez les variables d'environnement à partir du fichier de configuration
 load_status = dotenv.load_dotenv(config_file_path)
 if load_status is False:
     raise RuntimeError('Environment variables not loaded.')
 
-# Obtenez les variables d'environnement nécessaires pour la connexion
 URI = os.getenv("NEO4J_URI")
 AUTH = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
 
-# Connectez-vous à la base de données Neo4j
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
     driver.verify_connectivity()
     print("Successfull Connection to Database !")
     Create_nodes(csv_file_path)
-    # Fermez explicitement la connexion après avoir terminé
-    # driver.close()
